863295684d3bc8baea37619cd5c466bdf3d183b1
Egor <mizerov-2003@mail.ru>
2021-05-04 19:02:56 +0500

    reformat handlers and added function for start game on web client

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"testMascotGaming/internal/auth"
 	client2 "testMascotGaming/internal/client"
-	domain2 "testMascotGaming/internal/client/domain"
 	"testMascotGaming/internal/domain"
 	"testMascotGaming/internal/service"
 )
@@ -46,7 +45,6 @@ func (h *Handler) GetRouter() *gin.Engine {
 		h.InitAuthRoutes(api)
 		h.InitUserRoutes(api)
 		h.InitGamesRoutes(api)
-		//h.InitBankRoutes(api)
 	}
 
 	router.POST("/", func(ctx *gin.Context) {
@@ -78,124 +76,4 @@ func (h *Handler) GetRouter() *gin.Engine {
 	})
 
 	return router
-}
-
-func (h *Handler) getBalance(ctx *gin.Context, body []byte) {
-	var getBalanceReq domain2.GetBalanceRequest
-
-	err := json.Unmarshal(body, &getBalanceReq)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	balance, err := h.service.Balance.GetBalance(getBalanceReq.Params.PlayerName)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-	fmtBalance := int(balance * 100)
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"jsonrpc": "2.0",
-		"id":      getBalanceReq.Id,
-		"result": map[string]int{
-			"balance": fmtBalance,
-		},
-	})
-}
-
-func (h *Handler) withdrawAndDeposit(ctx *gin.Context, body []byte) {
-	var withdrawAndDepositReq withdrawAndDepositRequest
-
-	err := json.Unmarshal(body, &withdrawAndDepositReq)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	var userId = withdrawAndDepositReq.Params.PlayerName
-	var balance float64
-
-	transactionId, err := h.service.Transaction.CreateTransaction(userId, withdrawAndDepositReq.Params.TransactionRef, withdrawAndDepositReq.Params.Withdraw, withdrawAndDepositReq.Params.Deposit)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if withdrawAndDepositReq.Params.Deposit != 0 {
-		balance, err = h.service.Balance.Deposit(userId, float64(withdrawAndDepositReq.Params.Deposit)/100)
-		if err != nil {
-			errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-			return
-		}
-	}
-
-	if withdrawAndDepositReq.Params.Withdraw != 0 {
-		balance, err = h.service.Balance.Withdraw(userId, float64(withdrawAndDepositReq.Params.Withdraw)/100)
-		if err != nil {
-			errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-			return
-		}
-	}
-
-	fmtBalance := int(balance * 100)
-	result := map[string]interface{}{
-		"newBalance":    fmtBalance,
-		"transactionId": transactionId,
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"jsonrpc": "2.0",
-		"id":      withdrawAndDepositReq.Id,
-		"result":  result,
-	})
-}
-
-type withdrawAndDepositRequest struct {
-	Jsonrpc string
-	Method  string
-	Id      int
-	Params  withdrawAndDepositParams
-}
-
-type withdrawAndDepositParams struct {
-	PlayerName     string `json:"playerName"`
-	Withdraw       int
-	Deposit        int
-	TransactionRef string `json:"transactionRef"`
-}
-
-func (h *Handler) rollbackTransaction(ctx *gin.Context, body []byte) {
-	var rollbackTransactionReq rollbackTransactionRequest
-
-	err := json.Unmarshal(body, &rollbackTransactionReq)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	err = h.service.Rollback(rollbackTransactionReq.Params.TransactionRef)
-	if err != nil {
-		errorMessage(ctx, err, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"jsonrpc": "2.0",
-		"id":      rollbackTransactionReq.Id,
-		"result":  map[string]interface{}{},
-	})
-}
-
-type rollbackTransactionRequest struct {
-	Jsonrpc string
-	Method  string
-	Id      int
-	Params  rollbackTransactionParams
-}
-
-type rollbackTransactionParams struct {
-	PlayerName     string `json:"playerName"`
-	TransactionRef string `json:"transactionRef"`
 }
